a3e3d9069e88f0335104b9ba8673f241f8fabb33
San103 <palbanjessan15@gmail.com>
2024-05-29 17:00:31 +0800

    add Ui

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,152 +1,133 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
-import Dropdown from '@/Components/Dropdown.vue';
-import DropdownLink from '@/Components/DropdownLink.vue';
-import NavLink from '@/Components/NavLink.vue';
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+<script lang="ts" setup>
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import Dropdown from "@/Components/Dropdown.vue";
+import DropdownLink from "@/Components/DropdownLink.vue";
+import NavLink from "@/Components/NavLink.vue";
+import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
+import { Link } from "@inertiajs/vue3";
 
+import {
+    PieChartOutlined,
+    DesktopOutlined,
+    UserOutlined,
+    TeamOutlined,
+    FileOutlined,
+} from "@ant-design/icons-vue";
+
+const collapsed = ref<boolean>(false);
+const selectedKeys = ref<string[]>(["1"]);
 const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
     <div>
-        <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
-                <!-- Primary Navigation Menu -->
-                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="flex justify-between h-16">
-                        <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
-                                </Link>
-                            </div>
-
-                            <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
-                            </div>
-                        </div>
-
-                        <div class="hidden sm:flex sm:items-center sm:ms-6">
-                            <!-- Settings Dropdown -->
-                            <div class="ms-3 relative">
-                                <Dropdown align="right" width="48">
-                                    <template #trigger>
-                                        <span class="inline-flex rounded-md">
-                                            <button
-                                                type="button"
-                                                class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                            >
-                                                {{ $page.props.auth.user.name }}
-
-                                                <svg
-                                                    class="ms-2 -me-0.5 h-4 w-4"
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 20 20"
-                                                    fill="currentColor"
-                                                >
-                                                    <path
-                                                        fill-rule="evenodd"
-                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                        clip-rule="evenodd"
-                                                    />
-                                                </svg>
-                                            </button>
-                                        </span>
-                                    </template>
-
-                                    <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
-                                        <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
-                                        </DropdownLink>
-                                    </template>
-                                </Dropdown>
-                            </div>
-                        </div>
-
-                        <!-- Hamburger -->
-                        <div class="-me-2 flex items-center sm:hidden">
-                            <button
-                                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                            >
-                                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                                    <path
-                                        :class="{
-                                            hidden: showingNavigationDropdown,
-                                            'inline-flex': !showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16"
-                                    />
-                                    <path
-                                        :class="{
-                                            hidden: !showingNavigationDropdown,
-                                            'inline-flex': showingNavigationDropdown,
-                                        }"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Responsive Navigation Menu -->
-                <div
-                    :class="{ block: showingNavigationDropdown, hidden: !showingNavigationDropdown }"
-                    class="sm:hidden"
+        <a-layout style="min-height: 100vh">
+            <a-layout-sider v-model:collapsed="collapsed" collapsible>
+                <div class="logo" />
+                <a-menu
+                    v-model:selectedKeys="selectedKeys"
+                    theme="dark"
+                    mode="inline"
                 >
-                    <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
+                    <a-menu-item key="1">
+                        <pie-chart-outlined />
+                        <span>Dashboard</span>
+                    </a-menu-item>
+                    <!-- <a-menu-item key="2">
+                        <desktop-outlined />
+                        <span>Option 2</span>
+                    </a-menu-item> -->
+                    <a-sub-menu key="sub1">
+                        <template #title>
+                            <span>
+                                <user-outlined />
+                                <span>Transactions</span>
+                            </span>
+                        </template>
+                        <a-menu-item key="3">Tom</a-menu-item>
+                        <a-menu-item key="4">Bill</a-menu-item>
+                        <a-menu-item key="5">Alex</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub2">
+                        <template #title>
+                            <span>
+                                <team-outlined />
+                                <span>Sales</span>
+                            </span>
+                        </template>
+                        <a-menu-item key="6">Team 1</a-menu-item>
+                        <a-menu-item key="8">Team 2</a-menu-item>
+                    </a-sub-menu>
+                    <a-sub-menu key="sub3">
+                        <template #title>
+                            <span>
+                                <user-outlined />
+                                <span>Masterfile</span>
+                            </span>
+                        </template>
+                        <a-menu-item key="3">Tom</a-menu-item>
+                        <a-menu-item key="4">Bill</a-menu-item>
+                        <a-menu-item key="5">Alex</a-menu-item>
+                    </a-sub-menu>
+                    <a-menu-item key="9">
+                        <file-outlined />
+                        <span>File</span>
+                    </a-menu-item>
+                </a-menu>
+            </a-layout-sider>
+            <a-layout class="layout">
+                <a-layout-header>
+                    <div class="logo" />
+                    <a-menu
+                        v-model:selectedKeys="selectedKeys"
+                        theme="dark"
+                        mode="horizontal"
+                        :style="{ lineHeight: '64px' }"
+                    >
+                        <a-menu-item key="1">nav 1</a-menu-item>
+                        <a-menu-item key="2">nav 2</a-menu-item>
+                        <a-menu-item key="3">nav 3</a-menu-item>
+                    </a-menu>
+                </a-layout-header>
+                <a-layout-content style="padding: 0 50px">
+                    <a-breadcrumb style="margin: 16px 0">
+                        <a-breadcrumb-item>Home</a-breadcrumb-item>
+                        <a-breadcrumb-item>List</a-breadcrumb-item>
+                        <a-breadcrumb-item>App</a-breadcrumb-item>
+                    </a-breadcrumb>
+                    <div
+                        :style="{
+                            background: '#fff',
+                            padding: '24px',
+                            
+                        }"
+                    >
+                        <!-- Page Content -->
+                        <main>
+                            <slot />
+                        </main>
                     </div>
-
-                    <!-- Responsive Settings Options -->
-                    <div class="pt-4 pb-1 border-t border-gray-200">
-                        <div class="px-4">
-                            <div class="font-medium text-base text-gray-800">
-                                {{ $page.props.auth.user.name }}
-                            </div>
-                            <div class="font-medium text-sm text-gray-500">{{ $page.props.auth.user.email }}</div>
-                        </div>
-
-                        <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
-                            <ResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
-                            </ResponsiveNavLink>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-
-            <!-- Page Heading -->
-            <header class="bg-white shadow" v-if="$slots.header">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <slot name="header" />
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
-        </div>
+                </a-layout-content>
+                <a-layout-footer style="text-align: center">
+                    Ant Design ©2018 Created by Ant UED
+                </a-layout-footer>
+            </a-layout>
+        </a-layout>
     </div>
 </template>
+
+<style scoped>
+#components-layout-demo-side .logo {
+    height: 32px;
+    margin: 16px;
+    background: rgba(255, 255, 255, 0.3);
+}
+
+.site-layout .site-layout-background {
+    background: #fff;
+}
+[data-theme="dark"] .site-layout .site-layout-background {
+    background: #141414;
+}
+</style>
