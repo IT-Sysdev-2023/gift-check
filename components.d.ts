7a20533d50e481ee85ba907684d880323c981a89
WebmasterSensei <webmasteraturservice@gmail.com>
2024-08-14 14:28:57 +0800

    component kanding

--- components.d.ts
+++ components.d.ts
@@ -177,7 +177,7 @@ declare module 'vue' {
     PaperClipOutlined: typeof import('@ant-design/icons-vue')['PaperClipOutlined']
     PaymentType: typeof import('./resources/js/Components/Treasury/PaymentType.vue')['default']
     PendingPromoGcRequest: typeof import('./resources/js/Pages/Finance/PendingPromoGcRequest.vue')['default']
-    PendingRequest: typeof import('./resources/js/Pages/Treasury/Dashboard/PendingRequest.vue')['default']
+    PendingRequest: typeof import('./resources/js/Pages/Marketing/gcproductionrequest/PendingRequest.vue')['default']
     PendingRequestForm: typeof import('./resources/js/Pages/Marketing/gcproductionrequest/PendingRequestForm.vue')['default']
     PicRightOutlined: typeof import('@ant-design/icons-vue')['PicRightOutlined']
     PlazaMarcela: typeof import('./resources/js/Pages/Marketing/VerifiedGCperStore/PlazaMarcela.vue')['default']
