a79b404133752655ee98ac740d4048d71e063c01
hartzy0204 <harveybarace.main@gmail.com>
2024-08-29 11:39:38 -0700

    component

---
+++ resources/js/Components/AntDescItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <a-descriptions-item :label="props.label" :labelStyle="{ fontWeight: 'bold' }">{{ props.desc }}</a-descriptions-item>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    label: string;
+    desc: string;
+}>();
+</script>
