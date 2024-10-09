2a90b1619402c35b3a07fcb724c2fb0332a37eb2
hartzy0204 <harveybarace.main@gmail.com>
2024-10-08 17:49:00 -0700

    asdasdasfged

---
+++ resources/js/Pages/Custodian/ProductionIndex.vue
@@ -0,0 +1,46 @@
+<template>
+    <AuthenticatedLayout>
+        <a-card>
+            <a-table :data-source="record" :columns="column" size="small" bordered>
+                <template #bodyCell="{ column, record }">
+                    <template v-if="column.key === 'view'">
+                        <a-button @click="view(record.pe_id)">
+                            <template #icon>
+                                <ArrowsAltOutlined />
+                            </template>
+                            View Details
+                        </a-button>
+                    </template>
+                </template>
+            </a-table>
+        </a-card>
+
+        <production-approved-modal :id="peId" :width="1300" style="top: 40px" v-model:open="open" :data="details"/>
+
+    </AuthenticatedLayout>
+</template>
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+defineProps({
+    record: Object,
+    column: Array,
+});
+
+const open = ref(false);
+const details = ref({});
+const peId = ref('');
+
+
+const view = async (id) => {
+    try {
+        const { data } = await axios.get(route('custodian.production.details', id));
+        open.value = true;
+        peId.value = id;
+        details.value = data;
+    } catch (error) {
+
+    }
+}
+</script>
---
+++ resources/js/Pages/Marketing/ManagerDashboard.vue
@@ -0,0 +1,41 @@
+<template>
+    <a-row :gutter="[16, 16]">
+        <a-col :span="8">
+            <m-card
+            title="GC Production Request"
+            :pending="gcProductionRequest.pendingRequest"
+            :approved="gcProductionRequest.approvedRequest"
+            :cancelled="gcProductionRequest.cancelledRequest"
+            pRoute="marketing.pendingRequest.checker.pending.request"
+            aRoute="marketing.approvedRequest.approved.request"
+            cRoute="marketing.promoGcRequest.cancelled.list"
+            />
+        </a-col>
+        <a-col :span="8">
+           
+        </a-col>
+        <a-col :span="8">
+            <div class="mb-2">
+                <a-card title="Current Budget">
+                    <h2>₱ {{ currentBudget }}</h2>
+                </a-card>
+            </div>
+        </a-col>
+    </a-row>
+</template>
+
+<script>
+import Authenticatedlayout from "@/Layouts/AuthenticatedLayout.vue";
+import GcProductionReq from "./Card/GcProductionReq.vue";
+
+
+
+export default {
+    layout: Authenticatedlayout,
+
+    props: {
+        currentBudget: String,
+        gcProductionRequest: Object,
+    },
+};
+</script>
