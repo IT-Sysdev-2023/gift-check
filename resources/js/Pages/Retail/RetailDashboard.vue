841101f7c215e60b822c6695c4e336f5c7251b45
hartzy0204 <harveybarace.main@gmail.com>
2024-10-11 17:45:26 -0700

    loaders

--- resources/js/Pages/Retail/RetailDashboard.vue
+++ resources/js/Pages/Retail/RetailDashboard.vue
@@ -1,20 +1,29 @@
 <template>
     <AuthenticatedLayout>
+        <div>
+            <div class="flex justify-end">
+                <a-form-item class="mx-2" label="Revolving Fund">
+                    <a-input v-bind:value="'₱' + r_fund" readonly />
+                </a-form-item>
+                <a-form-item class="mx-2" label="Total GC">
+                    <a-input v-bind:value="'₱' + total" readonly />
+                </a-form-item>
+                <a-form-item class="mx-2" label="Current Store Budget">
+                    <a-input :max="r_fund" v-bind:value="'₱' + storeBudget" readonly />
+                </a-form-item>
+            </div>
+        </div>
+
         <a-row :gutter="[16, 16]">
             <a-col :span="8">
                 <store-gc-request-card :counts="countGcRequest" />
             </a-col>
             <a-col :span="8">
-                <available-gc :availableGc :total/>
+                <available-gc :availableGc :total />
             </a-col>
             <a-col :span="8">
-                <a-form-item label="Revolving Fund">
-                    <a-input v-bind:value="'₱'+r_fund" readonly/>
-                </a-form-item>
-                <a-form-item label="Current Store Budget">
-                    <a-input :max="r_fund" v-bind:value="'₱'+storeBudget" readonly/>
-                </a-form-item>
-                <sold-gc :soldGc/>
+
+                <sold-gc :soldGc />
             </a-col>
         </a-row>
     </AuthenticatedLayout>
@@ -27,10 +36,10 @@ export default {
     props: {
         countGcRequest: Object,
         availableGc: Object,
-        soldGc:Object,
-        total : Object,
-        r_fund:Object,
-        storeBudget:Object
+        soldGc: Object,
+        total: Object,
+        r_fund: Object,
+        storeBudget: Object
     }
 }
 </script>
