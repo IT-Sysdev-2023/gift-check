e287da386489596a27ada1c76321c1afca3de10e
Teofredo <teofredogamale6@gmail.com>
2024-10-17 13:30:57 +0800

    create store staff table

--- database/migrations/2024_05_09_092357_create_store_staff_table.php
+++ database/migrations/2024_05_09_092357_create_store_staff_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('store_staff', function (Blueprint $table) {
             $table->id();
-            $table->
             $table->timestamps();
         });
     }
