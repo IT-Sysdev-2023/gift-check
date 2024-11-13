64d0edddbdbb33419fb36a9705f2bb1de18a5662
hartzy0204 <harveybarace.main@gmail.com>
2024-11-13 14:18:49 -0800

    cancel spexgc table

---
+++ resources/js/Pages/Marketing/specialgc/Cancelledspexgc.vue
@@ -0,0 +1,38 @@
+<template>
+    <AuthenticatedLayout>
+        <a-card title="Cancelled Special GC Request">
+            <a-table size="small" bordered :dataSource="data" :columns="columns" />
+        </a-card>
+    </AuthenticatedLayout>
+</template>
+
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+
+defineProps({ data: Object });
+
+const columns = [
+    {
+        title: "Name",
+        dataIndex: "spexgc_num",
+        width: "10%",
+    },
+    {
+        title: "Date Request",
+        dataIndex: "spexgc_datereq",
+    },
+    {
+        title: "Customer",
+        dataIndex: "spcus_companyname",
+        ellipsis: "true",
+    },
+    {
+        title: "Date Cancelled",
+        dataIndex: "cancelledDate",
+    },
+    {
+        title: "Cancelled By",
+        dataIndex: "cancelledBy",
+    },
+];
+</script>
