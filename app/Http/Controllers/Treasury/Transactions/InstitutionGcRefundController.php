127848f4142b7b9b8709a7ea6c5fc189607f404c
San103 <palbanjessan15@gmail.com>
2024-11-19 13:42:42 +0800

    refund no function

--- app/Http/Controllers/Treasury/Transactions/InstitutionGcRefundController.php
+++ app/Http/Controllers/Treasury/Transactions/InstitutionGcRefundController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers\Treasury\Transactions;
 
 use App\Http\Controllers\Controller;
+use App\Models\Assignatory;
+use App\Services\Treasury\Transactions\InstitutionGcRefundService;
 use Illuminate\Http\Request;
 
 class InstitutionGcRefundController extends Controller
 {
+    public function __construct(public InstitutionGcRefundService $institutionGcRefundService)
+    {
+    }
     public function index(Request $request)
     {
-        return inertia('Treasury/Transactions/InstitutionGcRefund/InstitutionGcRefund',[
-            'title' => 'Institution Gc Refund',
-            'data' => [],
-            'column' => []
-        ]);
+        // $record = $this->institutionGcRefundService->index($request);
+
+        return inertia('Treasury/Transactions/InstitutionGcRefund/InstitutionGcRefund');
     }
 
     public function store(Request $request)
