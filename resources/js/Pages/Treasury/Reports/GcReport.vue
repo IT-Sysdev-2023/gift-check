1a4ac356f5182244790ea8d684e82959b2144e44
San103 <palbanjessan15@gmail.com>
2024-11-20 16:11:06 +0800

    gamayng changes gikan sa conflict

--- resources/js/Pages/Treasury/Reports/GcReport.vue
+++ resources/js/Pages/Treasury/Reports/GcReport.vue
@@ -49,7 +49,7 @@
                     <a-range-picker v-model:value="formState.date" />
                 </a-form-item>
                 <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
-                    <a-button type="primary" html-type="submit" :loading="loadingProgress"
+                    <a-button type="primary" html-type="submit"
                         >Generate</a-button
                     >
                 </a-form-item>
