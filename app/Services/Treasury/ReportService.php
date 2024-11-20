1b477327b323f098dbc1440dbb735ec7cbddc06a
San103 <palbanjessan15@gmail.com>
2024-11-20 16:06:43 +0800

    conflict

--- app/Services/Treasury/ReportService.php
+++ app/Services/Treasury/ReportService.php
@@ -5,15 +5,20 @@ namespace App\Services\Treasury;
 use App\Events\TreasuryReportEvent;
 use App\Jobs\GcReport;
 use App\Models\StoreEod;
+use App\Services\Documents\FileHandler;
+use App\Services\Documents\ImportHandler;
 use App\Services\Treasury\Reports\ReportsHandler;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 use App\Models\Store;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\LazyCollection;
+use Illuminate\Support\Str;
+use App\DashboardRoutesTrait;
 
 class ReportService extends ReportsHandler
 {
-
+	use DashboardRoutesTrait;
 
 	public static function reports() //storesalesreport.php
 	{
@@ -34,6 +39,7 @@ class ReportService extends ReportsHandler
 			"store" => 'required',
 			"date" => 'required_if:transactionDate,dateRange',
 		]);
+
 		GcReport::dispatch($request->all());
 	}
 
@@ -53,7 +59,7 @@ class ReportService extends ReportsHandler
 
 		return $pdf->output();
 	}
-	
+
 	private function handleEodRecords(Request $request)
 	{
 		$record = collect();
@@ -70,5 +76,38 @@ class ReportService extends ReportsHandler
 			'header' => $this->pdfEodHeaderDate(),
 			...$record,
 		];
+	}
+	public function generatedReports(Request $request)
+	{
+
+		$getFiles = (new ImportHandler())
+			->setFolder('Reports')
+			->getFilesFromDirectory($this->roleDashboardRoutes[$request->user()->usertype]);
+
+		
+		return inertia('Treasury/Reports/GeneratedReports', [
+			'files' => collect($getFiles)->transform(function ($item) {
+				$fileInfo = pathinfo($item);
+				$extension = $fileInfo['extension'];
+
+				$timestamp = Str::match('/\d{4}-\d{2}-\d{2}-\d{6}/', $item);
+				$generatedAt = Date::createFromFormat('Y-m-d-His', $timestamp);
+
+				return [
+					'file' => $item,
+					'filename' => Str::of(basename($item))->basename('.' . $extension),
+					'extension' => $extension,
+					'icon' => $extension === 'pdf' ? 'pdf.png' : 'excel.png',
+					'generatedAt' => $generatedAt->diffForHumans(),
+					'expiration' => $generatedAt->addDays(2)->diffForHumans(),
+				];
+			})->sortBy('generatedAt')->values()
+		]);
+	}
+	public function download(Request $request)
+	{
+		return (new ImportHandler())
+		->setFolder('Reports')
+		->downloadFile($request->file, true);
 	}
 }
