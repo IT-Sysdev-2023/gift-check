be318182013af815046920c4517aa67ed4b7b35c
San103 <palbanjessan15@gmail.com>
2024-11-21 08:46:20 +0800

    temporarily disable scheduler

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::job(new EodScheduler)->dailyAt('16:00');
+// Schedule::job(new EodScheduler)->dailyAt('16:00');
