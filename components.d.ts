1e7adc9f18e1983ea03943d253a865ca9f668cdc
Teofredo <teofredogamale6@gmail.com>
2024-11-27 16:59:00 +0800

    components

--- components.d.ts
+++ components.d.ts
@@ -106,6 +106,7 @@ declare module 'vue' {
     AppstoreOutlined: typeof import('@ant-design/icons-vue')['AppstoreOutlined']
     AProgress: typeof import('ant-design-vue/es')['Progress']
     ARadio: typeof import('ant-design-vue/es')['Radio']
+    ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
     ARangePicker: typeof import('ant-design-vue/es')['RangePicker']
     AreaChartOutlined: typeof import('@ant-design/icons-vue')['AreaChartOutlined']
