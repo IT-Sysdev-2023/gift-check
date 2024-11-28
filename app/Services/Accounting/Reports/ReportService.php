f6f5b6065f0091efb883bbf35cb33669a724d70f
San103 <palbanjessan15@gmail.com>
2024-11-28 09:10:26 +0800

    added validation in accounting report generation

--- app/Services/Accounting/Reports/ReportService.php
+++ app/Services/Accounting/Reports/ReportService.php
@@ -22,17 +22,24 @@ class ReportService
     public function specialGcReport(Request $request)
     {
         $request->validate([
-            'date' => 'array'
+            'date' => 'array',
         ]);
 
+        if (!self::hasApprovedSpgcRecord($request)) {
+            return response()->json(['error' => 'No Record Found on this date'], 404);
+        }
         SpgcApprovedReport::dispatch($request->only(['date', 'format']));
     }
-    
+
     public function specialGcReleasedReport(Request $request)
     {
         $request->validate([
             'date' => 'array'
         ]);
+
+        if (!self::hasReleasedSpgcRecord($request)) {
+            return response()->json(['error' => 'No Record Found on this date'], 404);
+        }
 
         SpgcReleasedReport::dispatch($request->only(['date', 'format']));
     }
@@ -43,5 +50,45 @@ class ReportService
         return inertia('Treasury/Reports/GeneratedReports', [
             'files' => $getFiles
         ]);
+    }
+
+    private static function hasApprovedSpgcRecord(Request $request)
+    {
+
+        $customer = SpecialExternalGcrequestEmpAssign::joinDataAndGetOnTables()
+            ->specialApproved($request->date)
+            ->groupBy(
+                'special_external_gcrequest.spexgc_datereq',
+                'special_external_gcrequest.spexgc_num',
+                'approved_request.reqap_date',
+                'special_external_customer.spcus_acctname',
+            )
+            ->exists();
+
+        $barcode = SpecialExternalGcrequestEmpAssign::joinDataBarTables()
+            ->specialApproved($request->date)
+            ->exists();
+
+        return $customer || $barcode;
+    }
+
+    private static function hasReleasedSpgcRecord(Request $request)
+    {
+
+        $customer = SpecialExternalGcrequestEmpAssign::joinDataAndGetOnTables()
+            ->specialReleased($request->date)
+            ->groupBy(
+                'special_external_gcrequest.spexgc_datereq',
+                'special_external_gcrequest.spexgc_num',
+                'approved_request.reqap_date',
+                'special_external_customer.spcus_acctname',
+            )
+            ->exists();
+
+        $barcode = SpecialExternalGcrequestEmpAssign::joinDataBarTables()
+            ->specialReleased($request->date)
+            ->exists();
+
+        return $customer || $barcode;
     }
 }
