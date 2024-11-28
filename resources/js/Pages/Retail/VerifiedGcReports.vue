ee74047c00f3d71440db74d927fadc6989a79e53
hartzy0204 <harveybarace.main@gmail.com>
2024-11-27 18:03:16 -0800

    loading2

--- resources/js/Pages/Retail/VerifiedGcReports.vue
+++ resources/js/Pages/Retail/VerifiedGcReports.vue
@@ -27,9 +27,36 @@
                     </a-row>
                 </a-col>
                 <a-col :span="16">
-                    <div class="flex justify-center">
-                        <a-form-item label="Generating">
-                            <a-progress type="dashboard" :percent="load" />
+                    <!-- Show progress when loading percentage is between 0 and 99 -->
+                    <div
+                        v-if="load.percentage > 0 && load.percentage < 99"
+                        class="flex justify-center"
+                    >
+                        <a-form-item label="Fetching Data">
+                            <a-progress
+                                type="dashboard"
+                                :percent="load.percentage"
+                            />
+                        </a-form-item>
+                    </div>
+
+                    <!-- Show spinner when percentage reaches 100 but PDF is not yet generated -->
+                    <div
+                        v-else-if="load.percentage == 100 && !generated"
+                        class="flex justify-center"
+                    >
+                        <a-form-item label="Generating PDF Please Wait">
+                            <a-spin />
+                        </a-form-item>
+                    </div>
+
+                    <!-- Show confirmation when PDF is generated -->
+                    <div
+                        v-else-if="load.percentage == 100 && generated"
+                        class="flex justify-center"
+                    >
+                        <a-form-item label="Generated">
+                            <CheckOutlined />
                         </a-form-item>
                     </div>
                 </a-col>
@@ -43,13 +70,15 @@
 
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
-import { router, useForm } from "@inertiajs/vue3";
-import { ref } from "vue";
+import { router, useForm, usePage } from "@inertiajs/vue3";
+import { onMounted, ref } from "vue";
 
+const page = usePage().props;
 const daterange = ref(null);
 const open = ref(false);
 const pdf = ref(null);
-const load = ref(0);
+const load = ref([]);
+const generated = ref(null);
 
 const getdate = (str, obj) => {
     daterange.value = obj;
@@ -65,9 +94,19 @@ const generatePdf = () => {
             onSuccess: (e) => {
                 pdf.value = `data:application/pdf;base64,${e.props.flash.stream}`;
                 open.value = true;
+                generated.value = true;
             },
             preserveState: true,
         }
     );
 };
+
+onMounted(() => {
+    window.Echo.private(
+        `verified-gc-report-pdf.${page.auth.user.user_id}`
+    ).listen(".verified-report-pdf", (e) => {
+        load.value = e;
+        console.log(e);
+    });
+});
 </script>
