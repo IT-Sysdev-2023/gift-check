b699c69046b3b75ac0c9313f102beb88a10c2eee
hartzy0204 <harveybarace.main@gmail.com>
2024-12-03 15:07:32 -0800

    ledger details query

--- resources/js/Pages/Retail/StoreLedger.vue
+++ resources/js/Pages/Retail/StoreLedger.vue
@@ -9,8 +9,18 @@
                 >
                     <template v-slot:bodyCell="{ column, record }">
                         <template v-if="column.dataIndex === 'view'">
-                            <a-button type="primary" @click="gcsalesdetails(record.sledger_ref)">
-                                <FolderFilled />
+                            <a-button
+                                type="primary"
+                                @click="
+                                    gcsalesdetails(
+                                        record.sledger_ref,
+                                        record.entry
+                                    )
+                                "
+                            >
+                                <FolderFilled />{{ record.sledger_ref }},{{
+                                    record.entry
+                                }}
                             </a-button>
                         </template>
                     </template>
@@ -34,6 +44,70 @@
                 <Pagination :datarecords="reval_data" class="mt-2" />
             </a-tab-pane>
         </a-tabs>
+
+        <a-modal v-model:open="type1" :footer="false" width="900px">
+            <a-card title="GC Entry Details">
+                <a-row :gutter="[16, 16]">
+                    <a-col :span="12">
+                        <a-form-item label="GC Receiving No">
+                            <a-input :value="type1data.srec_recid" readonly />
+                        </a-form-item>
+                        <a-form-item label="Date Received">
+                            <a-input :value="type1data.dateReceived" readonly />
+                        </a-form-item>
+                        <a-form-item label="Received By">
+                            <a-input :value="type1data.recBy" readonly />
+                        </a-form-item>
+                        <a-form-item label="Checked By">
+                            <a-input :value="type1data.checkby" readonly />
+                        </a-form-item>
+                    </a-col>
+
+                    <a-col :span="12">
+                        <a-table
+                            bordered
+                            :dataSource="type1table"
+                            :columns="type1columns"
+                            size="small"
+                        />
+                    </a-col>
+                </a-row>
+            </a-card>
+        </a-modal>
+        <a-modal v-model:open="type2" width="900px">
+            {{ type2data }}
+            <a-card>
+                <a-row :gutter="[16,16]">
+                    <a-col :span="12">
+                        <a-form-item label="Transaction #">
+                            <a-input :value="type2data.data.data.trans_number" readonly/>
+                        </a-form-item>
+                        <a-form-item label="Subtotal">
+                            <a-input :value="type2data.data.tpayment" readonly/>
+                        </a-form-item>
+                        <a-form-item label="Total Line Disc">
+                            <a-input :value="type2data.data.totlinedisc" readonly/>
+                        </a-form-item>
+                        <a-form-item label="Subtotal Disc">
+                            <a-input :value="type2data.data.data.docdisc" readonly/>
+                        </a-form-item>
+                        <a-form-item label="Total Payment">
+                            <a-input :value="type2data.data.tpayment" readonly/>
+                        </a-form-item>
+                        <a-form-item label="Cashier">
+                            <a-input :value="type2data.data.data.cashier" readonly/>
+                        </a-form-item>
+                        <a-form-item label="Amount Tender">
+                            <a-input readonly/>
+                        </a-form-item>
+                        <a-form-item label="Change">
+                            <a-input readonly/>
+                        </a-form-item>
+                    </a-col>
+                    <a-col :span="12"></a-col>
+                </a-row>
+            </a-card>
+        </a-modal>
     </AuthenticatedLayout>
 </template>
 
@@ -48,15 +122,49 @@ defineProps({
     reval_data: Object,
 });
 const activeKey = ref("1");
+const type1 = ref(false);
+const type2 = ref(false);
 
-const gcsalesdetails = (id) =>{
-    axios.get(route('retail.store_ledger.storeledgerdetails'),{
-        params:{
-            id: id
-        }
-    })
-}
+const type1data = ref({});
+const type1table = ref([]);
 
+const type2data = ref({});
+const type2table = ref([]);
+
+const gcsalesdetails = async (id, entry) => {
+    await axios
+        .get(route("retail.store_ledger.storeledgerdetails"), {
+            params: {
+                id: id,
+                entry: entry,
+            },
+        })
+        .then((r) => {
+            if (r.data.type == "1") {
+                type1data.value = r.data.data;
+                type1table.value = r.data.table;
+                type1.value = true;
+            } else if (r.data.type == "2") {
+                type2data.value = r.data
+                type2.value = true;
+            }
+        });
+};
+
+const type1columns = [
+    {
+        title: "Denomination",
+        dataIndex: "denomination",
+    },
+    {
+        title: "Qty",
+        dataIndex: "cnt",
+    },
+    {
+        title: "Total",
+        dataIndex: "total",
+    },
+];
 
 const ledgercolumns = [
     {
