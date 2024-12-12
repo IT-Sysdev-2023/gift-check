fa8c1c88135406840a2400dee57f850c0b26db96
San103 <palbanjessan15@gmail.com>
2024-12-12 17:55:24 +0800

    na build na

--- resources/js/Pages/StoreAccounting/VerifiedGCReport.vue
+++ resources/js/Pages/StoreAccounting/VerifiedGCReport.vue
@@ -362,7 +362,7 @@
         </div>
     </AuthenticatedLayout>
 </template>
-<script lang="ts" setup>
+<script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 // import { defineComponent } from '@vue/composition-api'
 import { ref } from "vue";
@@ -370,9 +370,7 @@ import { Modal } from "ant-design-vue";
 import { router } from "@inertiajs/core";
 import { HandleSelectTypes } from "@/types/treasury";
 
-defineProps<{
-    stores: HandleSelectTypes[];
-}>();
+defineProps(["stores"]);
 const GCDataTypeMonthly = ref({
     dataTypeMonthly: "",
     yearMonthly: "",
