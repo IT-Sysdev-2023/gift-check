4ab3825593a89a7f14f3219614c21d8ec47e2681
WebmasterSensei <webmasteraturservice@gmail.com>
2024-12-17 14:15:24 +0800

    eod configuration

--- resources/js/Pages/Finance/Cards/SpecialPendingRequestFinance.vue
+++ resources/js/Pages/Finance/Cards/SpecialPendingRequestFinance.vue
@@ -1,11 +1,16 @@
 <template>
     <a-card class="bg-card">
         <a-alert message="SPECIAL GC REQUEST" show-icon />
-        <div class="card" style="background-color: #EE4E4E">
-            <svg class="wave" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+        <div class="card" style="background-color: #ee4e4e">
+            <svg
+                class="wave"
+                viewBox="0 0 1440 320"
+                xmlns="http://www.w3.org/2000/svg"
+            >
                 <path
                     d="M0,256L11.4,240C22.9,224,46,192,69,192C91.4,192,114,224,137,234.7C160,245,183,235,206,213.3C228.6,192,251,160,274,149.3C297.1,139,320,149,343,181.3C365.7,213,389,267,411,282.7C434.3,299,457,277,480,250.7C502.9,224,526,192,549,181.3C571.4,171,594,181,617,208C640,235,663,277,686,256C708.6,235,731,149,754,122.7C777.1,96,800,128,823,165.3C845.7,203,869,245,891,224C914.3,203,937,117,960,112C982.9,107,1006,181,1029,197.3C1051.4,213,1074,171,1097,144C1120,117,1143,107,1166,133.3C1188.6,160,1211,224,1234,218.7C1257.1,213,1280,139,1303,133.3C1325.7,128,1349,192,1371,192C1394.3,192,1417,128,1429,96L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z"
-                    fill-opacity="1"></path>
+                    fill-opacity="1"
+                ></path>
             </svg>
 
             <div class="icon-container">
@@ -17,16 +22,30 @@
                 <p class="sub-text text-white">All Pending Request</p>
             </div>
             <a-badge :count="count.internal + count.external">
-                <a-popover v-model:open="open" trigger="click" :disabled="count.internal <= 0 || count.external <= 0">
+                <a-popover
+                    v-model:open="open"
+                    trigger="click"
+                    :disabled="count.internal <= 0 || count.external <= 0"
+                >
                     <template #content>
                         <a-badge class="m-2" :count="count.internal">
-                            <a-button :disabled="count.internal <= 0 " @click="pendingListLink('internal')" type="primary">Internal</a-button>
+                            <a-button
+                                :disabled="count.internal <= 0"
+                                @click="pendingListLink('internal')"
+                                type="primary"
+                                >Internal</a-button
+                            >
                         </a-badge>
                         <a-badge class="m-2" :count="count.external">
-                            <a-button :disabled="count.external <= 0" @click="pendingListLink('external')" type="primary">External</a-button>
+                            <a-button
+                                :disabled="count.external <= 0"
+                                @click="pendingListLink('external')"
+                                type="primary"
+                                >External</a-button
+                            >
                         </a-badge>
                     </template>
-                    <a-button  >
+                    <a-button>
                         <template #icon>
                             <FolderFilled />
                         </template>
@@ -36,11 +55,16 @@
             </a-badge>
         </div>
 
-        <div class="card" style="background-color: #34B3F1">
-            <svg class="wave" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+        <div class="card" style="background-color: #34b3f1">
+            <svg
+                class="wave"
+                viewBox="0 0 1440 320"
+                xmlns="http://www.w3.org/2000/svg"
+            >
                 <path
                     d="M0,256L11.4,240C22.9,224,46,192,69,192C91.4,192,114,224,137,234.7C160,245,183,235,206,213.3C228.6,192,251,160,274,149.3C297.1,139,320,149,343,181.3C365.7,213,389,267,411,282.7C434.3,299,457,277,480,250.7C502.9,224,526,192,549,181.3C571.4,171,594,181,617,208C640,235,663,277,686,256C708.6,235,731,149,754,122.7C777.1,96,800,128,823,165.3C845.7,203,869,245,891,224C914.3,203,937,117,960,112C982.9,107,1006,181,1029,197.3C1051.4,213,1074,171,1097,144C1120,117,1143,107,1166,133.3C1188.6,160,1211,224,1234,218.7C1257.1,213,1280,139,1303,133.3C1325.7,128,1349,192,1371,192C1394.3,192,1417,128,1429,96L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z"
-                    fill-opacity="1"></path>
+                    fill-opacity="1"
+                ></path>
             </svg>
 
             <div class="icon-container">
@@ -51,20 +75,28 @@
                 <p class="sub-text text-white">Approved Gc Request</p>
             </div>
             <a-badge :count="count.approve" :overflow-count="999">
-                <a-button @click="() => $inertia.get(route('finance.approvedGc.approved'))">
+                <a-button
+                    @click="
+                        () => $inertia.get(route('finance.approvedGc.approved'))
+                    "
+                >
                     <template #icon>
                         <FolderFilled />
                     </template>
                     View
                 </a-button>
             </a-badge>
-
         </div>
         <div class="card" style="background-color: #758694">
-            <svg class="wave" viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
+            <svg
+                class="wave"
+                viewBox="0 0 1440 320"
+                xmlns="http://www.w3.org/2000/svg"
+            >
                 <path
                     d="M0,256L11.4,240C22.9,224,46,192,69,192C91.4,192,114,224,137,234.7C160,245,183,235,206,213.3C228.6,192,251,160,274,149.3C297.1,139,320,149,343,181.3C365.7,213,389,267,411,282.7C434.3,299,457,277,480,250.7C502.9,224,526,192,549,181.3C571.4,171,594,181,617,208C640,235,663,277,686,256C708.6,235,731,149,754,122.7C777.1,96,800,128,823,165.3C845.7,203,869,245,891,224C914.3,203,937,117,960,112C982.9,107,1006,181,1029,197.3C1051.4,213,1074,171,1097,144C1120,117,1143,107,1166,133.3C1188.6,160,1211,224,1234,218.7C1257.1,213,1280,139,1303,133.3C1325.7,128,1349,192,1371,192C1394.3,192,1417,128,1429,96L1440,64L1440,320L1428.6,320C1417.1,320,1394,320,1371,320C1348.6,320,1326,320,1303,320C1280,320,1257,320,1234,320C1211.4,320,1189,320,1166,320C1142.9,320,1120,320,1097,320C1074.3,320,1051,320,1029,320C1005.7,320,983,320,960,320C937.1,320,914,320,891,320C868.6,320,846,320,823,320C800,320,777,320,754,320C731.4,320,709,320,686,320C662.9,320,640,320,617,320C594.3,320,571,320,549,320C525.7,320,503,320,480,320C457.1,320,434,320,411,320C388.6,320,366,320,343,320C320,320,297,320,274,320C251.4,320,229,320,206,320C182.9,320,160,320,137,320C114.3,320,91,320,69,320C45.7,320,23,320,11,320L0,320Z"
-                    fill-opacity="1"></path>
+                    fill-opacity="1"
+                ></path>
             </svg>
 
             <div class="icon-container">
@@ -75,7 +107,16 @@
                 <p class="sub-text text-white">All Cancelled Request</p>
             </div>
             <a-badge :count="count.cancel" :overflow-count="999">
-                <a-button @click="() => $inertia.get(route('finance.cancelledSpecialExternalGC.list'))">
+                <a-button
+                    @click="
+                        () =>
+                            $inertia.get(
+                                route(
+                                    'finance.cancelledSpecialExternalGC.list',
+                                ),
+                            )
+                    "
+                >
                     <template #icon>
                         <FolderFilled />
                     </template>
@@ -86,28 +127,28 @@
     </a-card>
 </template>
 <script>
-
 export default {
     props: {
         count: Array,
     },
 
-    methods:{
-        pendingListLink(type){
-            this.$inertia.get(route('finance.pendingGc.pending'),{
-                type: type
+    methods: {
+        pendingListLink(type) {
+            this.$inertia.get(route("finance.pendingGc.pending"), {
+                type: type,
             });
-        }
-    }
-}
+        },
+    },
+};
 </script>
 <style scoped>
 .bg-card {
-    box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
+    box-shadow:
+        rgba(9, 30, 66, 0.25) 0px 4px 8px -2px,
+        rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
 }
 
 .card {
-
     height: 65px;
     border-radius: 8px;
     margin-top: 7px;
