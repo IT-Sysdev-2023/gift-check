ab6660ee6bc4c5e1ade02c3cbcfc3c9fe345dd46
WebmasterSensei <webmasteraturservice@gmail.com>
2025-03-14 17:34:46 +0800

    changes in special

--- resources/js/Pages/Accounting/AccountingDashboard.vue
+++ resources/js/Pages/Accounting/AccountingDashboard.vue
@@ -4,11 +4,11 @@
             <a-col :span="8">
                 <m-card title="Special Gc Request" aRoute="custodian.approved.request" pRoute="accounting.pending.index"
                     :pending="count.pending">
-                    <a-button block type="primary" class="mb-2"
+                    <a-button style="width: 340px" type="primary" class="mb-2"
                         @click="router.get(route('accounting.payment.payment.gc'))">
                         Payment Gc
                     </a-button>
-                    <a-button block type="primary" class="mb-2"
+                    <a-button style="width: 340px" type="primary" class="mb-2"
                         @click="router.get(route('accounting.payment.viewing'))">
                         Payment Viewing
                     </a-button>
@@ -16,7 +16,8 @@
             </a-col>
             <a-col :span="8">
                 <a-card title="Institution GC Released" class="mb-5 uppercase">
-                    <inner-m-card label="Transactions" routeTo="treasury.transactions.institution.gc.sales.transaction" :count="data?.institutionGcSales" />
+                    <inner-m-card label="Transactions" routeTo="treasury.transactions.institution.gc.sales.transaction"
+                        :count="data?.institutionGcSales" />
                 </a-card>
             </a-col>
         </a-row>
--- resources/js/Pages/Accounting/Payment/PaymentIndex.vue
+++ resources/js/Pages/Accounting/Payment/PaymentIndex.vue
@@ -2,22 +2,27 @@
     <AuthenticatedLayout>
         <a-card>
             <a-table bordered :data-source="record.data" :columns="columns" size="small" :pagination="false">
-                <template #bodyCell="{column, record}">
+                <template #bodyCell="{ column, record }">
                     <template v-if="column.key === 'status'">
-                        <span v-if="record.spexgc_payment_stat === 'paid'">
-                            <a-tag color="blue">{{ record.spexgc_payment_stat }}</a-tag>
+                        <span v-if="record.spexgc_payment_stat === 'status'">
+                            <a-tag color="Green">{{ record.spexgc_payment_stat }}</a-tag>
                         </span>
                         <span v-else>
-                            <a-tag >{{ record.spexgc_payment_stat }}</a-tag>
+                            <a-tag>{{ record.spexgc_payment_stat }}</a-tag>
                         </span>
                     </template>
                     <template v-if="column.key === 'setup'">
-                        <a-button @click="() => $inertia.get(route('accounting.payment.setup', record.spexgc_id))">
-                            <template #icon>
-                                <FastForwardOutlined />
-                            </template>
-                            Setup Payment
-                        </a-button>
+                        <div v-if="record.spexgc_payment_stat == 'whole'">
+                            <a-tag color="green">Closed/Paid</a-tag>
+                        </div>
+                        <div v-else>
+                            <a-button @click="() => $inertia.get(route('accounting.payment.setup', record.spexgc_id))">
+                                <template #icon>
+                                    <FastForwardOutlined />
+                                </template>
+                                Setup Payment
+                            </a-button>
+                        </div>
                     </template>
                 </template>
             </a-table>
