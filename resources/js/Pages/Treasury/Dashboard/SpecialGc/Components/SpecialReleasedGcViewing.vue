426e9b8a7877b0d630f88665cd169b3b7c1f177e
hartzy0204 <harvey.barace.j@gmail.com>
2025-03-24 14:48:13 +0800

    soa

--- resources/js/Pages/Treasury/Dashboard/SpecialGc/Components/SpecialReleasedGcViewing.vue
+++ resources/js/Pages/Treasury/Dashboard/SpecialGc/Components/SpecialReleasedGcViewing.vue
@@ -18,22 +18,22 @@
                             <a-descriptions size="small" title="Requested Details" layout="horizontal" bordered>
                                 <a-descriptions-item style="width: 50%;" :span="3" label="Requisition No">{{
                                     record.data.spexgc_num
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Date Requested">{{
                                     record.data.spexgc_datereq
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Requested By">{{
                                     record.data.user
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Date Validity">{{
                                     record.data.spexgc_dateneed
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Remarks">{{
                                     record.data.approvedRequest?.reqap_remarks
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item label="Payment Type" :span="2">{{
                                     record.data.paymentTypeFormat
-                                    }}</a-descriptions-item>
+                                }}</a-descriptions-item>
                             </a-descriptions>
                             <a-descriptions size="small" title="Approved Details" class="mt-10" layout="horizontal"
                                 bordered>
@@ -45,13 +45,13 @@
                                 </a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Checked By">{{
                                     record.data.approvedRequest?.reqap_checkedby
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Prepared By">{{
                                     record.data.approvedRequest?.user.full_name
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Remarks">{{
                                     record.data.spexgc_remarks
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Approved By">{{
                                     record.data.approvedRequest
                                         ?.reqap_approvedby
@@ -65,27 +65,29 @@
                                 </a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Remarks">{{
                                     reviewed.reqap_remarks
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Reviewed By">{{
                                     reviewed.user?.full_name
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                             </a-descriptions>
-                            <a-descriptions title="Released Details" size="small" class="mt-10" layout="horizontal" bordered>
+                            <a-descriptions title="Released Details" size="small" class="mt-10" layout="horizontal"
+                                bordered>
                                 <a-descriptions-item style="width: 50%;" :span="3" label="Date Released">
                                     {{ dayjs(released.reqap_date) }}
                                 </a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Remarks">{{
                                     released.reqap_remarks
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Received By">{{
                                     record.data.spexgc_receviedby
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                                 <a-descriptions-item :span="3" label="Released By">{{
                                     released.user.full_name
-                                }}</a-descriptions-item>
+                                    }}</a-descriptions-item>
                             </a-descriptions>
                         </a-col>
-                    </a-row></a-tab-pane>
+                    </a-row>
+                </a-tab-pane>
                 <a-tab-pane key="2" tab="Barcodes" force-render>
                     <a-table bordered :data-source="barcodes.data" :columns="[{
                         title: 'Barcode',
@@ -115,7 +117,21 @@
                     </a-table>
                 </a-tab-pane>
             </a-tabs>
+            <div class="mt-5">
+                <div class="flex gap-5">
+                    <a-button>
+                        <FilePdfOutlined />Reprint
+                    </a-button>
+                    <a-button :loading="isloading" type="primary" @click="soaPrint">
+                        <FilePdfOutlined />Print SOA
+                    </a-button>
+                </div>
+            </div>
         </a-card>
+        <a-modal :open="isOpen" width="1000px" height="600">
+            <iframe :src="pdf" frameborder="0" width="100%" height="600"></iframe>
+        </a-modal>
+
     </AuthenticatedLayout>
 </template>
 <script lang="ts" setup>
@@ -128,12 +144,15 @@ import type { UploadFile } from "ant-design-vue";
 import { PageWithSharedProps } from "@/types/index";
 import { onProgress } from "@/Mixin/UiUtilities";
 
+const isOpen = ref(false);
+const isloading = ref(false)
 const props = defineProps<{
     title?: string;
     record: any;
     reviewed: any;
     released: any;
     barcodes: any;
+    id: string
 }>();
 const activeKey = ref("1");
 const page = usePage<PageWithSharedProps>().props;
@@ -146,4 +165,24 @@ const disabledDate = (current: Dayjs) => {
 const stream = ref(null);
 const openIframe = ref(false);
 const { openLeftNotification } = onProgress();
+
+const pdf = ref(null)
+
+
+const soaPrint = () => {
+    isloading.value = true
+    router.get(route('treasury.special.gc.soaPrint'), {
+        data: props.record,
+        barcode: props.barcodes,
+        released: props.released,
+        id: props.id
+    }, {
+        onSuccess: (r: any) => {
+            pdf.value = `data:application/pdf;base64,${r.props.flash.stream}`
+            isOpen.value = true
+            isloading.value = false
+        },
+        preserveState: true
+    });
+};
 </script>
