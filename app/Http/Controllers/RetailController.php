5c6a492223053c28a2cd4b270f1cd8bfe55dfb54
itsysdev2020 <itsysdev2020@gmail.com>
2025-04-01 11:43:26 +0800

    hey

--- app/Http/Controllers/RetailController.php
+++ app/Http/Controllers/RetailController.php
@@ -148,7 +148,7 @@ class RetailController extends Controller
 
     public function gcRequestsubmit(Request $request)
     {
-        
+
         $request->validate([
             'remarks' => 'required',
             'quantities' => 'required'
@@ -359,38 +359,19 @@ class RetailController extends Controller
 
     public function verificationIndex(Request $request)
     {
-            $data = $this->statusScanner->statusScanned($request);
+        $data = $this->statusScanner->statusScanned($request);
 
-            return inertia('Retail/Verification', [
-                'data' => $data->steps,
-                'success' => $data->success,
-                'notfound' => $data->barcodeNotFound,
-                'empty' => $data->empty,
-            ]);
-       
+        return inertia('Retail/Verification', [
+            'data' => $data->steps,
+            'success' => $data->success,
+            'notfound' => $data->barcodeNotFound,
+            'empty' => $data->empty,
+        ]);
     }
 
     public function submitVerify(Request $request)
     {
-         $st = Store::where('store_id', $request->user()->store_assigned)->first();
-
-        $driveLetter = 'Z:';
-        $networkPath =  rtrim($st->store_textfile_ip, '\\');
-        $username = $st->username;  // No extra quotes
-        $password = $st->new_password;
-
-        // Map the drive - note the quoting of username/password if needed
-        $command = "C:\\Windows\\System32\\net.exe use $driveLetter \"{$networkPath}\" /user:\"$username\" \"$password\" /persistent:yes 2>&1";
-
-
-        exec($command, $output, $return_var);
-
-
-        if ($return_var === 0) {
-            return $this->retail->submitVerify($request);
-        } else {
-            return inertia('ErrorInServer');
-        }
+        return $this->retail->submitVerify($request);
     }
     public function availableGcList(Request $request)
     {
